47f2f64d41bad34580e4ad9d409aa4ff44249801
Hassan Siddiqui <hassans@astro.princeton.edu>
2018-07-23 18:00:50 -0400

    Added uniqueness tests for title and id. Renamed some of the books->docs

--- docmanager.py
+++ docmanager.py
@@ -5,8 +5,9 @@ from flask import render_template
 from flask import request
 from flask import redirect
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import exists
 import datetime
-
+from typing import List
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "docdatabase.db"))
@@ -31,15 +32,33 @@ class Doc(db.Model):
 def home():
     if request.form:
         now = datetime.datetime.now()
-        book = Doc(id=request.form.get("id"), title=request.form.get("title"), author=request.form.get("author"), date=now.isoformat())
+
+        # Check that id and title are unique
+        identifier = request.form.get("id")
+        title = request.form.get("title")
+        warnings=[]
+        checkId(identifier, warnings)
+        checkTitle(title, warnings)
+        if warnings:
+            docs = Doc.query.all()
+            return render_template("home.html", warnings=warnings, docs=docs)
+
+        book = Doc(id=identifier, title=title, author=request.form.get("author"), date=now.isoformat())
         db.session.add(book)
         db.session.commit()
-    books = Doc.query.all()
-    return render_template("home.html", books=books)
+
+    docs = Doc.query.all()
+    return render_template("home.html", docs=docs)
 
 @app.route("/update", methods=["POST"])
 def update():
     newtitle = request.form.get("newtitle")
+    warnings = []
+    checkTitle(newtitle, warnings)
+    if warnings:
+        docs = Doc.query.all()
+        return render_template("home.html", warnings=warnings, docs=docs)
+
     newauthor = request.form.get("newauthor")
     sid = request.form.get("id")
     book = Doc.query.filter_by(id=sid).first()
@@ -55,6 +74,20 @@ def delete():
     db.session.delete(book)
     db.session.commit()
     return redirect("/")
+
+
+def checkId(identifier:str, warnings: List[str]):
+    # https://stackoverflow.com/questions/32938475/flask-sqlalchemy-check-if-row-exists-in-table/42920524
+    exists = db.session.query(db.exists().where(Doc.id == identifier)).scalar()
+    if exists:
+        warnings.append("Id: {} already exists".format(identifier))
+    return
+
+def checkTitle(title:str, warnings: List[str]):
+    exists = db.session.query(db.exists().where(Doc.title == title)).scalar()
+    if exists:
+        warnings.append("Title: {} already exists".format(title))
+    return
 
 if __name__ == "__main__":
     app.run(debug=True)
